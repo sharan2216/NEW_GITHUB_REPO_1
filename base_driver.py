26c95e5f71c5d80cfaddf3f584b8a4a092d2dce7
shashi <sharan88btm@gmail.com>
2023-10-05 11:41:45 +0530

    newly updated code 5

--- base_driver.py
+++ base_driver.py
@@ -5,10 +5,9 @@
 def method():
     print("base driver method1 added")
 
-<<<<<<< HEAD
+
 
 def method2():
     print("base driver method2 added")
 
-=======
->>>>>>> 07dfb99fd3d437d3c778615473e351e7152af1b8
+
