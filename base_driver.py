19a184c04422379b41f3efa40a18e04f03dfe3ea
shashi <sharan88btm@gmail.com>
2023-10-19 11:00:43 +0530

    new codes added in Main Branch

--- base_driver.py
+++ base_driver.py
@@ -6,11 +6,8 @@
 # def method():
 #     print("base driver method1 added")
 
+#
+# def test_method2():
+#     print("test method2 added by sdet2")
 
-def test_method2():
-    print("test method2 added by sdet2")
 
-
-def test_method3():
-    print("test method3 added")
-    
