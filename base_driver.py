308cce379d4e918500dae954274cb6c0a1c46c02
shashi <sharan88btm@gmail.com>
2023-10-19 11:13:04 +0530

    base_driver file updated now

--- base_driver.py
+++ base_driver.py
@@ -3,14 +3,6 @@
 # some lines added
 # some lines added
 # some lines added
-# def method():
-#     print("base driver method1 added")
-
-#
-# def test_method2():
-#     print("test method2 added by sdet2")
-
-
 
 def test_method2():
     print("test method2 added by sdet2")
