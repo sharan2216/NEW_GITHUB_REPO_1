f7c692c14616f1d68ad621c9338232f810aa22c7
shashi <sharan88btm@gmail.com>
2023-10-19 10:54:42 +0530

    new code added 1

--- base_driver.py
+++ base_driver.py
@@ -7,14 +7,10 @@
 #     print("base driver method1 added")
 
 
+def test_method2():
+    print("test method2 added by sdet2")
+
+
 def test_method3():
     print("test method3 added")
     
-
-
-# def test_method():
-#     print("test method added")
-
-
-def test_method2():
-    print("test method2 added by sdet2")
