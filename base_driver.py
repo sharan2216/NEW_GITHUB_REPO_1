ab49e23ae9023804db841ede9a9d4f247b1dc662
shashi <sharan88btm@gmail.com>
2023-10-19 11:17:14 +0530

    basedriver file gets updated

--- base_driver.py
+++ base_driver.py
@@ -7,9 +7,17 @@
 #     print("base driver method1 added")
 
 
-def method2():
-    print("base driver method2 added")
+def test_method1():
+    print("base driver method1 added")
 
 
 def test_method2():
-    print("test method2 added by sdet2")
+    print("test method2 added by sdet1")
+
+
+def test_method3():
+    print("test method3 added by sdet1")
+
+
+def test_method4():
+    print("test method4 added by sdet1")
